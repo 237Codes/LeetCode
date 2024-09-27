a6c108b0bc105cbc95ef2118378083b5c9be02f1
ManyakaCodes <104553245+237Codes@users.noreply.github.com>
2024-09-26 20:38:30 -0400

    LC-is-subsequence [Runtime: N/A; Memory: N/A]

---
+++ string/is-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        index = 0
+        for letter in t:
+            if letter == s[index]:
+                index += 1
+            if index == len(s):
+                return True
+        return False
+
