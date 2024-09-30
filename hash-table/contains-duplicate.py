1102298607d3103f5a4544163e71182f9426aeea
ManyakaCodes <104553245+237Codes@users.noreply.github.com>
2024-09-30 00:04:11 -0400

    LC-contains-duplicate [Runtime: 409 ms; Memory: 34.8 MB]

--- hash-table/contains-duplicate.py
+++ hash-table/contains-duplicate.py
@@ -1,11 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #Create a Map to hold values in list
         seen = {}
-        #Traverse List and Seacrch map if number already exist, else add it to map
         for num in nums:
             if num in seen:
                 return True
-            else:
-                seen[num] = None
-        return False
+            seen[num] =1
+        return False        
